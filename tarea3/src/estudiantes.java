03d63d53791b4232c996cd1cfc43fca147827b8f
JsoNJ552 <114025870+JsoNJ552@users.noreply.github.com>
2022-10-13 16:23:29 -0600

    Add files via upload

---
+++ tarea3/src/estudiantes.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.JOptionPane; 
+
+/**
+ *
+ * @author jason
+ * 
+ */
+public class estudiantes {
+    String nombre;
+    int edad;
+    String Grado;
+    int cedula;
+    String direcccion;
+    String nombrepadre;
+    String nombremadre;
+    String becado;
+    String Asistencia;
+    
+    
+    
+     public static void main(String[] args) {
+         
+         
+         }
+            
+     
+         
+         
+     }
+
+    
+
+    
+
---
+++ tarea3/src/personalcocina.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jason
+ */
+public class personalcocina {
+    String nombre;
+    int edad;
+    String puestoenespecifico;
+    int cedula;
+    String direcccion;
+    String Asistencia;
+    int salario;
+    
+    
+    public void agregar(){
+        
+    }
+    public void eliminar(){
+        
+    }
+    public void modificar(){
+        
+    }
+    public void calcularsalario(){
+        
+    }
+    public void avisarferiado(){
+        
+    }
+    
+}
